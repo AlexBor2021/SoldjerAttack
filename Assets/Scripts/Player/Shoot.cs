b8c201fcf5f5c29b3f69d2cf1b7870297baec6a6
Алексей <93997737+AlexBor2021@users.noreply.github.com>
2023-01-14 22:01:05 +0300

    стрельба игрока

---
+++ Assets/Scripts/Player/Shoot.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+    [SerializeField] private ParticleSystem _effectShoot;
+    [SerializeField] private Player _player;
+
+    private Enemy _enemy;
+    private Coroutine _damageTakeCor;
+
+    private void OnEnable()
+    {
+        _effectShoot.Stop();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            if (_enemy == null && enemy.Health > 0)
+            {
+                _enemy = enemy;
+                _damageTakeCor = StartCoroutine(TakeDamage());
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Enemy>() == _enemy && _enemy != null)
+        {
+            StopCoroutine(_damageTakeCor);
+            _enemy = null;
+        }
+    }
+
+    private IEnumerator TakeDamage()
+    {
+        while (_enemy.Health > 0)
+        {
+            _player.transform.LookAt(_enemy.transform.position);
+            _enemy.TakeDamage(_damage);
+            _effectShoot.Play();
+            yield return new WaitForSeconds(2f);
+        }
+
+        _enemy = null;
+        yield return null;
+    }
+}
