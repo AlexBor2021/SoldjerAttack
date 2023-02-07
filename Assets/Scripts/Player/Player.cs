b1f9343c23029761df09b20a26fdee5ede2bee14
Алексей <93997737+AlexBor2021@users.noreply.github.com>
2023-02-07 10:38:49 +0300

    no message

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,25 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Agava.YandexGames;
+using UnityEngine.Audio;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject _superPlayer;
     [SerializeField] private GameObject _playerEssy;
     [SerializeField] private int _timeSuperPlayer;
+    [SerializeField] private AudioMixer _audioMixer;
 
     private bool _isSuperPlayer;
     private const string _offSuperPlayer = "OffSuperPlayer";
-   
+    private const string _masterSound = "MasterSound";
 
     public void SwitshSuperPlayer()
     {
-        if (_isSuperPlayer == false)
+        VideoAd.Show(OffMusicVolume, Revard, OnMusicVolume);
+
+        void OffMusicVolume()
         {
-            _isSuperPlayer = true;
-            _playerEssy.SetActive(false);
-            _superPlayer.SetActive(true);
-            Invoke(_offSuperPlayer, _timeSuperPlayer);
+            Time.timeScale = 0;
+            _audioMixer.SetFloat(_masterSound, -80);
+        }
+        void OnMusicVolume()
+        {
+            Time.timeScale = 1;
+            _audioMixer.SetFloat(_masterSound, 0);
+        }
+
+        void Revard()
+        {
+            if (_isSuperPlayer == false)
+            {
+                _isSuperPlayer = true;
+                _playerEssy.SetActive(false);
+                _superPlayer.SetActive(true);
+                Invoke(_offSuperPlayer, _timeSuperPlayer);
+            }
         }
     }
 
--- Assets/Scripts/SupotCargo.cs
+++ Assets/Scripts/SupotCargo.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Agava.YandexGames;
+using UnityEngine.Audio;
 
 public class SupotCargo : MonoBehaviour
 {
     [SerializeField] private GameObject _prefabCargo;
     [SerializeField] private ParticleSystem _effectCreate;
+    [SerializeField] private AudioMixer _audioMixer;
 
     private Transform _player;
+    private const string _masterSound = "MasterSound";
 
     private void OnEnable()
     {
@@ -16,10 +20,26 @@ public class SupotCargo : MonoBehaviour
 
     public void CreateCargo()
     {
-        Vector3 position = new Vector3(_player.position.x+Random.Range(-3, 3), _player.position.y, _player.position.z + Random.Range(2, 5));
+        VideoAd.Show(OffMusicVolume, Revard, OnMusicVolume);
 
-        Instantiate(_effectCreate, position, Quaternion.identity);
-        Instantiate(_prefabCargo, position, Quaternion.identity);
+        void OffMusicVolume()
+        {
+            Time.timeScale = 0;
+            _audioMixer.SetFloat(_masterSound, -80);
+        }
+        void OnMusicVolume()
+        {
+            Time.timeScale = 1;
+            _audioMixer.SetFloat(_masterSound, 0);
+        }
+
+        void Revard()
+        {
+            Vector3 position = new Vector3(_player.position.x + Random.Range(-3, 3), _player.position.y, _player.position.z + Random.Range(2, 5));
+
+            Instantiate(_effectCreate, position, Quaternion.identity);
+            Instantiate(_prefabCargo, position, Quaternion.identity);
+        }
     }
 
 
