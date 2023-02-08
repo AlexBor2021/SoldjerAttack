ef939826731afb628bcc539d8720798f4c5cef7c
NoroMedafe <danya1515@inbox.ru>
2023-02-08 17:17:58 +0300

    no message

--- Assets/Scripts/PlayerPower.cs
+++ Assets/Scripts/PlayerPower.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerPower : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private int _coast = 200;
+    private float _moneyValue;
     private MoverPlayer _player;
+    private BarGold _money;
 
     private void Start()
     {
         _player = FindObjectOfType<MoverPlayer>();
+        _money = FindObjectOfType<BarGold>();
+
+        _text.text = _coast.ToString();
     }
 
     public void UpdateSpeed()
     {
-        _player.UppSpeed(1);
+        if (_money.GoldCount >= _coast)
+        {
+            _player.UppSpeed(1);
+            _money.GiveGald(_coast);
+        }
     }
 }
