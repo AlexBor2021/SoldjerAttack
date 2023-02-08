4ecba0782efa798e43537bf2a19eef4ecb4386ac
NoroMedafe <danya1515@inbox.ru>
2023-02-08 17:34:22 +0300

    no message

--- Assets/Scripts/BarGold.cs
+++ Assets/Scripts/BarGold.cs
@@ -7,8 +7,13 @@ public class BarGold : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _gold;
 
-    private int _goldCount;
+    private int _goldCount = 400;
     public int GoldCount => _goldCount;
+
+    private void Start()
+    {
+        _gold.text = _goldCount.ToString();
+    }
     public void TakeGold(int number)
     {
         _goldCount += number;
@@ -17,6 +22,7 @@ public class BarGold : MonoBehaviour
 
     public void GiveGald(int number)
     {
+        Debug.Log("улучшил");
         _goldCount -= number;
         _gold.text = _goldCount.ToString();
     }
