06c241ed5ce2e4536d0c1cdec90bee08723d7224
Алексей <93997737+AlexBor2021@users.noreply.github.com>
2023-02-13 20:31:43 +0300

    no message

---
+++ Assets/Scripts/LevelTextCanvas.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelTextCanvas : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _numberLevel;
+
+    private void OnEnable()
+    {
+        _numberLevel.text = (SceneManager.GetActiveScene().buildIndex - 1).ToString();
+    }
+}
